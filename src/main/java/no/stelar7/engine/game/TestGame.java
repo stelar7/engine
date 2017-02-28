08243ac2fcbc906be487bc6279a7ae15e868c4fb
stelar7 <dudedbz@gmail.com>
2017-02-28 11:08:53 +0100

    Add a model class for encapsulating buffers
    Add OpenGL call log to TestGame

--- src/main/java/no/stelar7/engine/game/TestGame.java
+++ src/main/java/no/stelar7/engine/game/TestGame.java
@@ -1,29 +1,25 @@
 package no.stelar7.engine.game;
 
 import no.stelar7.engine.EngineUtils;
-import no.stelar7.engine.rendering.buffers.*;
+import no.stelar7.engine.rendering.Model;
 import no.stelar7.engine.rendering.shaders.*;
 
 import java.nio.*;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 
 
 public class TestGame implements Game
 {
     
-    private VertexArrayObject  vao = new VertexArrayObject();
-    private VertexBufferObject vbo = new VertexBufferObject(GL_ARRAY_BUFFER);
-    private VertexBufferObject ibo = new VertexBufferObject(GL_ELEMENT_ARRAY_BUFFER);
+    // GL call log: https://hastebin.com/raw/foqahevigi
     
-    private ShaderProgram program;
+    private ShaderProgram shader;
+    private Model         model;
     
     public TestGame()
     {
-        EngineUtils.log("// Shader start");
-        
         Shader vertexShader = new Shader(GL_VERTEX_SHADER);
         vertexShader.setSource(EngineUtils.readShader("basic.vert"));
         vertexShader.compile();
@@ -31,51 +27,23 @@ public class TestGame implements Game
         Shader fragmentShader = new Shader(GL_FRAGMENT_SHADER);
         fragmentShader.setSource(EngineUtils.readShader("basic.frag"));
         fragmentShader.compile();
-        
-        EngineUtils.log("// Program start");
-        
-        program = new ShaderProgram(vertexShader, fragmentShader);
-        
-        EngineUtils.log("// VAO start");
-        // bind VAO
-        vao.generate();
-        vao.bind();
-        
-        EngineUtils.log("// VBO start");
-        // bind VBO
-        vbo.generate();
-        vbo.bind();
-        vbo.setData(FloatBuffer.wrap(new float[]{-1, -1, 0, 1, -1, 0, 0, 1, 0}));
-        
-        EngineUtils.log("// Bind VAO and VBO start");
-        // set pointers
-        vao.enableAttribIndex(0);
-        vao.setPointer(0, 3);
-        
-        EngineUtils.log("// IBO start");
-        // bind IBO
-        ibo.generate();
-        ibo.bind();
-        ibo.setData(IntBuffer.wrap(new int[]{0, 1, 2}));
-        
-        
-        // unbind vao
-        vao.unbind();
+    
+        shader = new ShaderProgram(vertexShader, fragmentShader);
+        model = new Model(FloatBuffer.wrap(new float[]{-1, -1, 0, 1, -1, 0, 0, 1, 0}), IntBuffer.wrap(new int[]{0, 1, 2}));
         
         glClearColor(.8f, 0, 0, 0);
         EngineUtils.log("glClearColor(%s, %s, %s, %s)", .8f, 0, 0, 0);
     }
     
-    private float scale;
+    private float scale = 1;
     
     @Override
     public void update()
     {
-        EngineUtils.log("// Update start");
-        program.bind();
+        shader.bind();
         scale += 0.01f;
-        program.setUniform1f("scale", (float) Math.sin(scale));
-        program.unbind();
+        shader.setUniform1f("scale", (float) Math.sin(scale));
+        shader.unbind();
     }
     
     
@@ -84,15 +52,12 @@ public class TestGame implements Game
     @Override
     public void render()
     {
-        EngineUtils.log("// Render start");
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         EngineUtils.log("glClear(%s | %s)", EngineUtils.glTypeToString(GL_COLOR_BUFFER_BIT), EngineUtils.glTypeToString(GL_DEPTH_BUFFER_BIT));
         
-        // bind shader and VAO
-        program.bind();
-        vao.bind();
+        shader.bind();
+        model.bind();
         
-        // Render
         
         counter = ++counter % 3;
         
@@ -110,37 +75,32 @@ public class TestGame implements Game
             EngineUtils.log("glVertex3f(1, -1, 0)");
             EngineUtils.log("glVertex3f(0, 1, 0)");
             EngineUtils.log("glEnd()");
-            
-            
         }
         
         
         if (counter == 1)
         {
             // This call does nothing?
-            glDrawArrays(GL_TRIANGLES, 0, 4);
-            EngineUtils.log("glDrawArrays(%s, %s, %s)", EngineUtils.glTypeToString(GL_TRIANGLES), 0, 4);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            EngineUtils.log("glDrawArrays(%s, %s, %s)", EngineUtils.glTypeToString(GL_TRIANGLES), 0, 3);
         }
         
         if (counter == 2)
         {
             // This call does nothing?
-            glDrawElements(GL_TRIANGLES, 4, GL_UNSIGNED_INT, 0);
-            EngineUtils.log("glDrawElements(%s, %s, %s, %s)", EngineUtils.glTypeToString(GL_TRIANGLES), 4, EngineUtils.glTypeToString(GL_UNSIGNED_INT), 0);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+            EngineUtils.log("glDrawElements(%s, %s, %s, %s)", EngineUtils.glTypeToString(GL_TRIANGLES), 3, EngineUtils.glTypeToString(GL_UNSIGNED_INT), 0);
         }
         
         
-        // unbind shader and vao
-        vao.unbind();
-        program.unbind();
+        model.unbind();
+        shader.unbind();
     }
     
     @Override
     public void delete()
     {
-        ibo.delete();
-        vbo.delete();
-        vao.delete();
-        program.delete();
+        model.delete();
+        shader.delete();
     }
 }
---
+++ src/main/java/no/stelar7/engine/rendering/Model.java
@@ -0,0 +1,51 @@
+package no.stelar7.engine.rendering;
+
+import no.stelar7.engine.rendering.buffers.*;
+
+import java.nio.*;
+
+import static org.lwjgl.opengl.GL15.*;
+
+public class Model
+{
+    
+    private final VertexArrayObject  vao = new VertexArrayObject();
+    private final VertexBufferObject vbo = new VertexBufferObject(GL_ARRAY_BUFFER);
+    private final VertexBufferObject ibo = new VertexBufferObject(GL_ELEMENT_ARRAY_BUFFER);
+    
+    public Model(FloatBuffer vert, IntBuffer ind)
+    {
+        vao.generate();
+        vbo.generate();
+        ibo.generate();
+        
+        
+        vao.bind();
+        
+        vbo.bind();
+        vbo.setData(vert);
+        vao.enableAttribIndex(0);
+        vao.setPointer(0, 3);
+        
+        ibo.bind();
+        ibo.setData(ind);
+        
+    }
+    
+    public void bind()
+    {
+        vao.bind();
+    }
+    
+    public void unbind()
+    {
+        vao.unbind();
+    }
+    
+    public void delete()
+    {
+        ibo.delete();
+        vbo.delete();
+        vao.delete();
+    }
+}
